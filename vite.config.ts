75be524f451a7a02ff71297755a4ea87b3ae34f2
UserGit1995 <simonearico10@gmail.com>
2026-08-24 20:57:32 +0200

    Remove server preset and add tsconfig paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from '@tanstack/start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  server: {
-    preset: 'vercel',
-  },
   vite: {
-    resolve: {
-      tsconfigPaths: true,
-    },
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+    ],
   },
 });
