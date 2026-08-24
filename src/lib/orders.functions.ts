c7ff488301620a57ae204d1421d51b08ecd5a4cf
UserGit1995 <simonearico10@gmail.com>
2026-08-24 18:00:04 +0200

    Update orders.functions.ts

--- src/lib/orders.functions.ts
+++ src/lib/orders.functions.ts
@@ -1,5 +1,5 @@
 import { createServerFn } from '@tanstack/start';
-import db from '../db';
+import db from '../db.server';
 
 export const getOrders = createServerFn({ method: 'GET' }).handler(async () => {
   const { data, error } = await db
