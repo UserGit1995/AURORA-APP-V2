55b46b49a7bde79e1949775e68ac91cb6dd8ce66
UserGit1995 <simonearico10@gmail.com>
2026-08-24 17:58:55 +0200

    Update import path for db module to db.server

--- src/lib/products.functions.ts
+++ src/lib/products.functions.ts
@@ -1,5 +1,5 @@
 import { createServerFn } from '@tanstack/start';
-import db from '../db';
+import db from '../db.server';
 
 export const getProducts = createServerFn({ method: 'GET' }).handler(async () => {
   const { data, error } = await db
