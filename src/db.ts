865dbd63a6c394001d64b08ab108cc1e478dfe54
UserGit1995 <simonearico10@gmail.com>
2026-08-24 17:43:34 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL || process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY || process.env.SUPABASE_PUBLISHABLE_KEY || process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey =
+  process.env.SUPABASE_ANON_KEY ||
+  process.env.SUPABASE_PUBLISHABLE_KEY ||
+  process.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Configurazione Supabase mancante nelle Environment Variables.');
+  throw new Error('Configurazione Supabase mancante nelle Environment Variables su Vercel.');
 }
 
-export const myFunc = createServerFn().validator(supabaseUrl, supabaseKey);
-export default supabase;
+export const db = createClient(supabaseUrl, supabaseKey);
+export default db;
