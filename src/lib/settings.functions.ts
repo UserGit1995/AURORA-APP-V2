778a61ca3055e3ef53a0231c25f5f2e470aa2ab3
UserGit1995 <simonearico10@gmail.com>
2026-08-24 17:59:20 +0200

    Update settings.functions.ts

--- src/lib/settings.functions.ts
+++ src/lib/settings.functions.ts
@@ -1,5 +1,5 @@
 import { createServerFn } from '@tanstack/start';
-import db from '../db';
+import db from '../db.server';
 
 export const getSettings = createServerFn({ method: 'GET' }).handler(async () => {
   const { data, error } = await db
